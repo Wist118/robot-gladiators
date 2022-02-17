55007d7458fbcb7620d2a72e3ad801f4b606fb9a
William Stock <grim118@hotmail.com>
2022-02-17 10:20:28 -0600

    fixed enemy health

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,10 +14,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    //Alert players they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
 
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    while(enemyHealth > 0) {
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -64,8 +63,14 @@ var fight = function(enemyName) {
 } else {
     window.alert("You need to choose a valid option. Try again!");
 }
+    }
 };
+    
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
+
+//fight();
